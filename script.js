4bd602d3bbde12757d34beb0c826c9eff3610cee
dzmods <167025473+dzmods@users.noreply.github.com>
2024-10-24 23:53:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,170 +1,64 @@
-(function() {
-    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+var num = 1
+var objetos = {"Objects": []};
+var objItens = []
 
-    // Main
-    initHeader();
-    initAnimation();
-    addListeners();
+function SpawnObject(nomeEvento, pos, yaws) {
 
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = {x: width/2, y: height/2};
+  var cords = pos.split(" ")
+  var outras = yaws.split(" ")
+  var yam = outras[0];
+  var pit = outras[1];
+  var rol = outras[2];
+  //Broken down new loot co-ordinates
+  var x = cords[0];
+  var y = cords[1];
+  var z = cords[2];
+  var eventspawn = "";
 
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
+  nomeEvento2 = nomeEvento.replace('_', '')
 
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
+  let evento = ('        <event name="Vehicle'+nomeEvento2+''+num+'">\n            <nominal>1</nominal>\n            <min>1</min>\n            <max>1</max>\n            <lifetime>300</lifetime>\n            <restock>0</restock>\n            <saferadius>5</saferadius> <!-- The saferadius is the distance away from the player position that this event can spawn. -->\n            <distanceradius>5</distanceradius> <!-- The distanceradius is the minimum distance away from other similar event. -->\n            <cleanupradius>5</cleanupradius> <!-- The cleanupradius is the distance away from the player position that the event will despawn after lifetime ticks down. -->\n            <flags deletable="0" init_random="0" remove_damaged="0"/>\n            <position>fixed</position>\n            <limit>mixed</limit>\n            <!-- <limit>child</limit> -->\n            <active>1</active>\n            <children>\n                <child lootmax="0" lootmin="0" max="1" min="1" type="'+nomeEvento+'"/>\n            </children>\n        </event>\n');
 
-        points = [];
-        for(var x = 0; x < width; x = x + width/20) {
-            for(var y = 0; y < height; y = y + height/20) {
-                var px = x + Math.random()*width/20;
-                var py = y + Math.random()*height/20;
-                var p = {x: px, originX: px, y: py, originY: py };
-                points.push(p);
-            }
-        }
+  a = yam;
+  if (a < 0) {
+      if(a*-1 > 180)
+        a = 360 - (a*-1);
+  }
+  eventspawn = (`<event name="Vehicle${nomeEvento2}${num}">\n    <pos x="${x}" y="${y}" z="${z}" a="${a}" />\n</event>`);
 
-        for(var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for(var j = 0; j < points.length; j++) {
-                var p2 = points[j];
-                if(!(p1 == p2)) {
-                    var placed = false;
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
+  var objeto = {
+      "name": nomeEvento,
+      "pos": [
+        parseFloat(x),
+        parseFloat(y),
+        parseFloat(z)
+      ],
+      "ypr": [
+        parseFloat(a),
+        parseFloat(pit),
+        parseFloat(rol)
+      ]
+    };
 
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-                }
-            }
-            p1.closest = closest;
-        }
+  objetos.Objects.push(objeto);
 
-        for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
-            points[i].circle = c;
-        }
-    }
+  // console.log(JSON.stringify(objeto, null, '\t'));
 
-    function addListeners() {
-        if(!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
-        }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
+  $("#events").val($("#events").val() + evento );
+  $("#eventspawn").val($("#eventspawn").val() + eventspawn );
+  $("#jsonObject").val($("#jsonObject").val() + JSON.stringify(objetos, null, '\t'));
 
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        } else if (e.clientX || e.clientY) {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-    }
+} 
 
-    function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
-    }
+$("#enviar").click(function() {
+  $("#events").val("");
+  $("#eventspawn").val("");
+  $("#jsonObject").val("");
+  var lines = $('#dayzpc').val().split('\n');
+  for(var i = 0;i < lines.length;i++) {
+    eval(lines[i])
+  }
 
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        largeHeader.style.height = height+'px';
-        canvas.width = width;
-        canvas.height = height;
-    }
+  // console.log(JSON.stringify(objetos, null, '\t'));
 
-    function initAnimation() {
-        animate();
-        for(var i in points) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if(animateHeader) {
-            ctx.clearRect(0,0,width,height);
-            for(var i in points) {
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
-                    points[i].active = 0.02;
-                    points[i].circle.active = 0.1;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
-                }
-
-                drawLines(points[i]);
-                points[i].circle.draw();
-            }
-        }
-        requestAnimationFrame(animate);
-    }
-
-    function shiftPoint(p) {
-        TweenLite.to(p, 1+1*Math.random(), {x: p.originX - 50 + Math.random() * 100, y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut, onComplete: function() { shiftPoint(p); }});
-    }
-
-    function drawLines(p) {
-        if(!p.active) return;
-        for(var i in p.closest) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,' + p.active + ')';
-            ctx.stroke();
-        }
-    }
-
-    function Circle(pos, rad, color) {
-        var _this = this;
-
-        (function() {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
-
-        this.draw = function() {
-            if(!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,' + _this.active + ')';
-            ctx.fill();
-        };
-    }
-
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
-
-})();
+});
