5b1b8d84d19ab94319fec4302704838c34293e45
dzmods <167025473+dzmods@users.noreply.github.com>
2024-11-24 01:27:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Logo
     var logoContainer = document.getElementById("logoContainer");
     var logoImg = document.createElement("img");
-    logoImg.src = "https://cdn.discordapp.com/attachments/1180529719607828523/1200567635889377410/BANNER.png?ex=662c2a3d&is=6619b53d&hm=32b7ecfd3c7eb256c295daba11f8e6b5807753fc9a5654371eddd0caa88e17c1";
+    logoImg.src = "https://cdn.discordapp.com/attachments/1309891025371664426/1310054016277024799/DZ.png?ex=6743d261&is=674280e1&hm=4b256520bb3e1bf7140797b826882191dc08ce9832dfdb5e22d1fe911032c844&";
     logoImg.alt = "Logo";
     logoContainer.appendChild(logoImg);
 
